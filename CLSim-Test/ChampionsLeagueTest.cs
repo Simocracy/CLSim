73d655de6fd22d3246046e38f5cfd6c7eead9108
Gerald Siegert <gerald.siegert@outlook.com>
2017-06-14 17:49:22 +0200

    added test project

---
+++ CLSim-Test/ChampionsLeagueTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simocracy.CLSim.Simulation;
+
+namespace Simocracy.CLSim.Test
+{
+	[TestClass]
+	public class ChampionsLeagueTest
+	{
+
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+		}
+	}
+}
---
+++ CLSim-Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CLSim.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CLSim.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8797d52-4bcd-466d-a065-3012f7f69e91")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
