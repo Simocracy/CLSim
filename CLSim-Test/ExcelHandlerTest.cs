43015a34709d3c27e1602be03069ae20692b3d09
Gerald Siegert <gerald.siegert@outlook.com>
2017-06-29 19:16:10 +0200

    added unit test for excel handler

---
+++ CLSim-Test/ExcelHandlerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simocracy.CLSim.Football.Base;
+using Simocracy.CLSim.Football.UAFA;
+using Simocracy.CLSim.IO;
+using System.IO;
+
+namespace Simocracy.CLSim.Test
+{
+    [TestClass]
+    public class ExcelHandlerTest
+    {
+        [TestMethod]
+        public void TestFileCreation()
+        {
+            var coeff1 = new Coefficient(new FootballTeam("{{FRC}} BAM", 12))
+            {
+                Won = 10,
+                Drawn = 2
+            };
+            coeff1.RoundsPlayed.Add(ETournamentRound.CLGroupStage);
+            coeff1.RoundsPlayed.Add(ETournamentRound.CLRoundOf16);
+            coeff1.RoundsPlayed.Add(ETournamentRound.CLQuarterFinals);
+
+            var coeff2 = new Coefficient(new FootballTeam("{{UNAS}} Seattle", 12))
+            {
+                Won = 9,
+                Drawn = 1
+            };
+            coeff2.RoundsPlayed.Add(ETournamentRound.CLGroupStage);
+            coeff2.RoundsPlayed.Add(ETournamentRound.CLRoundOf16);
+            coeff2.RoundsPlayed.Add(ETournamentRound.CLQuarterFinals);
+            coeff2.RoundsPlayed.Add(ETournamentRound.CLSemiFinals);
+
+            var excelHandler = new ExcelHandler();
+            excelHandler.WriteHeaders("2053/54");
+            excelHandler.WriteTeamCoefficientLine(coeff1);
+            excelHandler.WriteTeamCoefficientLine(coeff2);
+            excelHandler.CloseFile("testfile.xlsx");
+
+            Assert.IsTrue(File.Exists("testfile.xlsx"));
+        }
+    }
+}
