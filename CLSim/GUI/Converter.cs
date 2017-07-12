6a79284ef88396df8a2d253634fe9b62477dbeea
Gerald Siegert <gerald.siegert@outlook.com>
2017-07-12 20:54:56 +0200

    added IsNullConverter

--- CLSim/GUI/Converter.cs
+++ CLSim/GUI/Converter.cs
@@ -71,4 +71,20 @@ namespace Simocracy.CLSim.GUI
         }
     }
 
+    /// <summary>
+    /// Converts a null to true
+    /// </summary>
+    public class IsNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value == null);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException($"{nameof(IsNullConverter)} can only be used OneWay.");
+        }
+    }
+
 }
