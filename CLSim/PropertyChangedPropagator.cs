86cb5037835dab4dbebb0d105583a5a9aa78fc81
Gerald Siegert <gerald.siegert@outlook.com>
2017-07-17 10:49:03 +0200

    added PropertyChangedPropagator

---
+++ CLSim/PropertyChangedPropagator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simocracy.CLSim
+{
+    /// <summary>
+    /// Helper class for notify readonly properties in referenced classes
+    /// </summary>
+    /// <remarks>See https://stackoverflow.com/questions/43653750 </remarks>
+    public static class PropertyChangedPropagator
+    {
+        public static PropertyChangedEventHandler Create(string sourcePropertyName, string dependantPropertyName, Action<string> raisePropertyChanged)
+        {
+            var infiniteRecursionDetected = false;
+            return (sender, args) =>
+            {
+                try
+                {
+                    if (args.PropertyName != sourcePropertyName) return;
+                    if (infiniteRecursionDetected)
+                    {
+                        throw new InvalidOperationException("Infinite recursion detected");
+                    }
+                    infiniteRecursionDetected = true;
+                    raisePropertyChanged(dependantPropertyName);
+                }
+                finally
+                {
+                    infiniteRecursionDetected = false;
+                }
+            };
+        }
+    }
+}
