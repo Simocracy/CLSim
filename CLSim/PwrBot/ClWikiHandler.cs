796f37c8e1379052c82274ef62f7c6311e8880c0
Gerald Siegert <gerald.siegert@outlook.com>
2017-08-07 20:42:33 +0200

    added information for formatting variables on cl wiki export

--- CLSim/PwrBot/ClWikiHandler.cs
+++ CLSim/PwrBot/ClWikiHandler.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows.Media;
 using DotNetWikiBot;
+using Simocracy.CLSim.Football.UAFA;
 using Simocracy.PwrBot;
 using SimpleLogger;
 
@@ -18,6 +21,14 @@ namespace Simocracy.CLSim.PwrBot
         public const string CurrentClRawPageTitle = "CLSim/UAFA CL Vorlage 2054";
         public const string ClPageTitlePrefix = "UAFA Champions League";
 
+        public static readonly Color ColorGroupStage = Color.FromRgb(0xe5, 0xb1, 0xb1);
+        public static readonly Color ColorRoundOf32 = Color.FromRgb(0xff, 0xda, 0xda);
+        public static readonly Color ColorRoundOf16 = Color.FromRgb(0xff, 0xdf, 0xb1);
+        public static readonly Color ColorQuarterFinals = Color.FromRgb(0xff, 0xff, 0xb1);
+        public static readonly Color ColorSemiFinals = Color.FromRgb(0xb1, 0xee, 0xb1);
+        public static readonly Color ColorFinal = Color.FromRgb(0x9f, 0xd3, 0x9f);
+        public static readonly Color ColorWinner = Color.FromRgb(0xb1, 0xee, 0xff);
+
         #endregion
 
         #region Members
@@ -32,6 +43,13 @@ namespace Simocracy.CLSim.PwrBot
         #endregion
 
         #region Constructors
+
+        /// <summary>
+        /// Creates a new cl wiki handler instance
+        /// </summary>
+        public ClWikiHandler()
+        {
+        }
 
         /// <summary>
         /// Creates a new cl wiki handler instance
@@ -89,7 +107,7 @@ namespace Simocracy.CLSim.PwrBot
 
         #endregion
 
-        #region Methods
+        #region Page Methods
 
         /// <summary>
         /// Gets the raw page code for the CL site
@@ -184,24 +202,65 @@ namespace Simocracy.CLSim.PwrBot
             return false;
         }
 
-        public bool CreatePageContent()
+        /// <summary>
+        /// Creates the page content for the given <see cref="ChampionsLeague"/>
+        /// </summary>
+        /// <param name="cl">The CL complete simulated instance</param>
+        /// <returns>True if successfully</returns>
+        /// <remarks>
+        /// The Variables needed in Raw Page:
+        ///  0: season number
+        ///  1: start year (4 digits)
+        ///  2: final year (2 digits)
+        ///  3: full final date string
+        ///  4: final city
+        ///  5: winner team
+        ///  6: last winner
+        ///  7: group stage out color
+        ///  8: round of 16 out color
+        ///  9: quarter finals out color
+        /// 10: semi finals out color
+        /// 11: final color
+        /// 12: winner color
+        /// 13: last winner out color
+        /// 14-89: team out color / team with remarks
+        /// 90-91: MAC-PV team out color /team with remarks
+        /// 
+        /// </remarks>
+        public bool CreatePageContent(ChampionsLeague cl)
         {
-            SimpleLog.Info($"Create content for page {PageTitle}.");
+            SimpleLog.Info($"Create content for page {PageTitle} with Champions League {cl.Season}.");
+
+            if(cl.Final == null || !cl.Final.IsSimulated)
+            {
+                SimpleLog.Warning($"The given CL {cl.Season} is not completly simulated.");
+                return false;
+            }
 
             try
             {
+                var sb = new StringBuilder();
+
+
+                PageContent = sb.ToString();
 
                 SimpleLog.Info($"Page content for {PageTitle} created.");
                 return true;
             }
             catch(Exception e)
             {
-                SimpleLog.Error($"Error during creating page content for {PageTitle}");
+                SimpleLog.Error($"Error during creating page content for {PageTitle}.");
                 SimpleLog.Error(e.ToString());
             }
 
             return false;
         }
+
+        #endregion
+
+        #region Utility Methods
+
+
 
         #endregion
 
